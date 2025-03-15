32be03a04ac762ddb2aa3fea94c96b28e747be5e
MBashov <galacticos.mb@gmail.com>
2025-03-15 11:50:31 +0100

    Extract code into jsx components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
-import Header from "./components/Header"
+import Bottomblock from "./components/bottom-block/BottomBlock"
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+import Page1Block from "./components/page1-block/Page1Block"
+import TopBlok1 from "./components/top-block/TopBlock1"
+import './css/form.css'
+import './css/grid.css'
+import './css/ie.css'
+// import './css/reset.css'
+// import './css/slider.css'
+import './css/style.css'
+import './css/superfish.css'
 
 function App() {
 
   return (
     <>
       <Header />
+      < TopBlok1 />
+      < Page1Block />
+      < Bottomblock />
+      < Footer />
     </>
   )
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container_12">
+                <div className="grid_12">
+                    <div className="socials">
+                        {" "}
+                        <a href="#" /> <a href="#" /> <a href="#" /> <a href="#" />{" "}
+                    </div>
+                    <p>
+                        Pet Club © 2045 | <a href="#">Privacy Policy</a> | Design by:{" "}
+                        <a href="http://www.templatemonster.com/">TemplateMonster.com</a>
+                    </p>
+                </div>
+                <div className="clear" />
+            </div>
+        </footer>
+
+    );
+}
