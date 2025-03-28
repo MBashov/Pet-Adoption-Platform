4381b363b70dd1944bddd40280bdd4b6bd5357de
MBashov <galacticos.mb@gmail.com>
2025-03-28 18:53:23 +0100

    Show spinner when logout

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router";
 
 import { useLogout } from "../../api/authApi";
+import Spinner from "../spinner/Spinner";
 
 export default function Logout() {
     const { isLoggedOut } = useLogout();
 
     return isLoggedOut
         ? <Navigate to='/' />
-        : null; //TODO show spinner
+        : <Spinner />
 }
