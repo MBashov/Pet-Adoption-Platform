a8d4cddd411b04a2a90b03cff6d929edb5de2ab3
MBashov <galacticos.mb@gmail.com>
2025-04-21 11:41:27 +0200

    Ad data validation when register

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -12,18 +12,32 @@ export default function Register() {
     const { register } = useRegister();
     const { authHandler, } = useUserContext();
     const [email, setEmail] = useState('');
-    const [passMismatch, setPassMismatch] = useState(false);
     const [emailError, setEmailError] = useState(false);
+    const [inValidEmail, setInValidEmail] = useState(false);
+    const [passMismatch, setPassMismatch] = useState(false);
+    const [shortPass, setShortPass] = useState(false);
 
     const handleFocus = () => {
         setEmailError(false);
         setPassMismatch(false);
+        setShortPass(false);
     };
 
     const registerHandler = async (_, formData) => {
 
         const { email, password, confirmPassword } = Object.fromEntries(formData);
         setEmail(email);
+
+        if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gm)) {
+            setInValidEmail(true);
+            setEmailError(true);
+            return;
+        }
+
+        if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$/)) {
+            setShortPass(true);
+            return;
+        }
 
         if (password !== confirmPassword) {
             setPassMismatch(true);
@@ -67,10 +81,10 @@ export default function Register() {
                             placeholder="maria@email.com"
                             defaultValue={email}
                             onFocus={handleFocus}
-                            required
                             className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ${emailError ? "border-red-500 focus:ring-red-500" : "focus:ring-blue-500"
                                 }`}
                         />
+                        {inValidEmail && <p className="text-red-900 text-sm mt-1">Invalid Email</p>}
                     </div>
 
                     <div>
@@ -80,10 +94,10 @@ export default function Register() {
                             name="password"
                             id="register-password"
                             onFocus={handleFocus}
-                            required
-                            className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ${passMismatch ? "border-red-500 focus:ring-red-500" : "focus:ring-blue-500"
+                            className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ${passMismatch || shortPass ? "border-red-500 focus:ring-red-500" : "focus:ring-blue-500"
                                 }`}
                         />
+                        {shortPass && <p className="text-red-900 text-sm mt-1">Password must be 8+ chars with upper, lower, and a number.</p>}
                     </div>
 
                     <div>
