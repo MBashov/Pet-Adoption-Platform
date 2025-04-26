3a1de8fb013ebd174652f97616730567667489cb
MBashov <galacticos.mb@gmail.com>
2025-04-26 19:18:25 +0200

    Add docker file for server

--- server/server.js
+++ server/server.js
@@ -1792,7 +1792,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = process.env.PORT || 3030;
 
     server.listen(port);
 
